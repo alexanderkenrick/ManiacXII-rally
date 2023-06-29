a6ea4ea3f07402bffb08ae413bdfe9292673ca7e
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-06-29 19:18:57 +0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -60,11 +61,11 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\Models\Account
      */
     protected function create(array $data)
     {
-        return User::create([
+        return Account::create([
             'username' => $data['username'],
             'email' => $data['email'],
             'name' => $data['name'],
