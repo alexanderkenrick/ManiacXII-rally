e81c249e7bd8ca686fc0f6ab643d56501eb73e5c
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-06-30 19:55:35 +0700

    Call Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        $this->call([
+           AccountSeeder::class,
+           TeamSeeder::class,
+           PostSeeder::class
+        ]);
     }
 }
