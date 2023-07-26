ef626310512f19b63d31b97bc4aa7f7c7c49e940
notkillua07 <112600181+notkillua07@users.noreply.github.com>
2023-07-26 16:02:27 +0700

    timer

--- resources/views/treasure.blade.php
+++ resources/views/treasure.blade.php
@@ -56,7 +56,7 @@
                                         Timer
                                     </div>
                                     <div class="col d-flex justify-content-end">
-                                        <span>05:00</span>
+                                        <span id="timer">05:00</span>
                                     </div>
                                 </div>
                             </div>
@@ -66,12 +66,13 @@
                                 <button class="button" id="button-reset">Reset</button>
                             </div>
                         </div>
-                        
+
                         {{-- Inventory --}}
                         <div class="inventory-section p-3 mt-4 ">
                             <div class="item">
                                 <div class="image-container">
-                                    <img src="{{ asset('/img/treasure/shovel.png') }}" alt="" class="item-image" id="shovel-image">
+                                    <img src="{{ asset('/img/treasure/shovel.png') }}" alt="" class="item-image"
+                                        id="shovel-image">
                                 </div>
                                 <div class="text-container px-3">
                                     <h2>Shovel</h2>
@@ -83,7 +84,8 @@
                             </div>
                             <div class="item">
                                 <div class="image-container">
-                                    <img src="{{ asset('/img/treasure/thief.png') }}" alt="" class="item-image" id="thief-image">
+                                    <img src="{{ asset('/img/treasure/thief.png') }}" alt="" class="item-image"
+                                        id="thief-image">
                                 </div>
                                 <div class="text-container px-3">
                                     <h2>Thief Bag</h2>
@@ -95,7 +97,8 @@
                             </div>
                             <div class="item">
                                 <div class="image-container">
-                                    <img src="{{ asset('/img/treasure/angel.png') }}" alt="" class="item-image" id="angel-image">
+                                    <img src="{{ asset('/img/treasure/angel.png') }}" alt="" class="item-image"
+                                        id="angel-image">
                                 </div>
                                 <div class="text-container px-3">
                                     <h2>Angel Card</h2>
@@ -125,7 +128,63 @@
             </div>
         </div>
     </div>
+    <script>
+        var timer;
+        var second = 300;
+        var running = false;
+        $(document).on('click', '#button-start', function() {
+            if (!running) {
+                $("#timer").css('display', 'inline');
+                running = true;
+                timer = setInterval(function() {
+                    second--;
+                    // $("#timer").text(second);
+                    showTimer(second);
+                    if (second <= 0) {
+                        $("#timer").text('Waktu Habis');
+                        // alert('Waktu Habis');
+                        Swal.fire({
+                            icon: 'warning',
+                            title: "Waktu Habis"
+                        });
+                        running = false;
+                        clearInterval(timer);
+                        second = 300;
+                    }
+                }, 1000);
+            }
+        });
+        
+        $(document).on('click', '#button-pause', function() {
+            // $("#timer").text(second);
+            showTimer(second);
+            running = false
+            clearInterval(timer);
+        });
 
+        $(document).on('click', '#button-reset', function() {
+            $("#timer").text('05:00');
+            running = false;
+            clearInterval(timer);
+            second = 300;
+        });
+
+        function showTimer(second) {
+            let minute = second / 60;
+            let seconds = second % 60;
+
+            let minuteString = parseInt(minute).toString();
+            let secondString = seconds.toString();
+            if (minuteString.length == 1) {
+                minuteString = "0" + minuteString;
+            }
+            if (secondString.length == 1) {
+                secondString = "0" + secondString;
+            }
+
+            $("#timer").text(`${minuteString}:${secondString}`);
+        }
+    </script>
 @endsection
 
 @section('script')
